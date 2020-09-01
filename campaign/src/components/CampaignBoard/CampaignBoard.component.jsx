cee3b84cc16a22a26b9707719e4bb452b67d35b6
Priyanshu Tyagi <priyanshutyagi@Priyanshus-MacBook-Air.local>
2020-09-01 10:05:44 +0530

    Camapign Board component to display the Campaigns

--- campaign/src/components/CampaignBoard/CampaignBoard.component.jsx
+++ campaign/src/components/CampaignBoard/CampaignBoard.component.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import LanguageSelector from '../LangauageSelector/LangaugeSelector.component';
+import Translator from '../translator/translator';
+import defaultMsg from '../LangauageSelector/defaultMessages';
+import CampaignSelect from '../campaign-select/CampaignSelect.component'
 
 function Main() {
         return (
-            <LanguageSelector><br/>
-            
+            <LanguageSelector>
+                <br/>
+            <div>
+                <div className="container mr-auto pl-5">
+                    <h1
+                        style={{ color: "#2b416c" }}><b>{Translator('header',defaultMsg.msg.err)}</b></h1>
+                </div>
+                <br />
+                <div className="container mr-auto pl-5">
+                    <CampaignSelect />
+                </div>
+            </div>
             </LanguageSelector>
         );
 }
